6e6a7f47c230bff715422d0cb7482f6366f26f27
Eduardo Antunes Campos de Sousa <eacsousa@yahoo.com.br>
2014-01-22 18:39:19 +0000

    Comando which
    git-tfs-id: [https://nest.visualstudio.com/defaultcollection]$/PSUtils;C9

--- PSUtils.psm1
+++ PSUtils.psm1
@@ -8,36 +8,46 @@ $paths = (
 
 $fsiPath = ($paths | ? { Test-Path $_ })[0]
 
-
+## F# Scripts
 function Change-NugetRefs {
     $fsiArgs = & { Join-Path $PSScriptRoot Change-NugetRefs.fsx; $args };
     & ( $fsiPath ) $fsiArgs
 }
 
 
+## Extenal PS1 Scripts
 iex ([String]::Join([Environment]::NewLine, (gc (Join-Path $PSScriptRoot Foreach-Parallel._ps1))))
 iex ([String]::Join([Environment]::NewLine, (gc (Join-Path $PSScriptRoot Set-VS2012._ps1))))
 iex ([String]::Join([Environment]::NewLine, (gc (Join-Path $PSScriptRoot Set-VS2013._ps1))))
 iex ([String]::Join([Environment]::NewLine, (gc (Join-Path $PSScriptRoot prompt._ps1))))
+iex ([String]::Join([Environment]::NewLine, (gc (Join-Path $PSScriptRoot Get-FindLocation._ps1))))
 
+# Aliases
 Set-Alias vs2012 Set-VS2012
 Set-Alias vs2013 Set-VS2013
+Set-Alias which Get-FindLocation
 
+# GNU Win32 Aliases
 Set-Alias sed (Join-Path $PSScriptRoot 'GnuWin32\sed.exe')
 Set-Alias gzip (Join-Path $PSScriptRoot 'GnuWin32\gzip.exe')
 Set-Alias wget (Join-Path $PSScriptRoot 'GnuWin32\wget.exe')
 Set-Alias zip (Join-Path $PSScriptRoot 'GnuWin32\zip.exe')
 Set-Alias unzip (Join-Path $PSScriptRoot 'GnuWin32\unzip.exe')
 
-Export-ModuleMember -alias vs2012
-Export-ModuleMember -alias vs2013
-
+# Export scripts
 Export-ModuleMember -function Foreach-Parallel
 Export-ModuleMember -function Change-NugetRefs
 Export-ModuleMember -function Set-VS2012
 Export-ModuleMember -function Set-VS2013
 Export-ModuleMember -function prompt
+Export-ModuleMember -function Get-FindLocation
 
+#Export aliases
+Export-ModuleMember -alias vs2012
+Export-ModuleMember -alias vs2013
+Export-ModuleMember -alias which
+
+#Export GNU Win32 Aliases
 Export-ModuleMember -alias sed
 Export-ModuleMember -alias gzip
 Export-ModuleMember -alias wget
