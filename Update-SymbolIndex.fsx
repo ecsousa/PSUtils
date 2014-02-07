1c9be4d94427453ed4a1b35fbde277e21eaafcca
Eduardo Antunes Campos de Sousa <eacsousa@yahoo.com.br>
2014-02-07 20:00:34 +0000

    Evolução na indexação de simbolos
    git-tfs-id: [https://nest.visualstudio.com/defaultcollection]$/PSUtils;C32

--- Update-SymbolIndex.fsx
+++ Update-SymbolIndex.fsx
@@ -10,8 +10,6 @@ open System.Text.RegularExpressions
 open Microsoft.TeamFoundation.Client
 open Microsoft.TeamFoundation.VersionControl.Client
 
-exception NotFoundInTFS of string list
-
 if fsi.CommandLineArgs.Length < 2 then
     printfn "É necessário infomar o(s) arquivo(s) .pdb a ser(em) indexado(s)"
     Environment.Exit(1)
@@ -63,77 +61,101 @@ let makeSrcsrv pdb =
     let serverName (wi: WorkspaceInfo) =
         "_" + (Regex.Replace(wi.ServerUri.ToString(), "[^\w]", "_")).ToUpper()
 
-    seq {
+    let indexed = ref false
 
-        yield "SRCSRV: ini ------------------------------------------------"
-        yield "VERSION=3"
-        yield "INDEXVERSION=2"
-        yield "VERCTRL=Team Foundation Server"
-        yield (sprintf "DATETIME=%s" (DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")))
-        yield "SRCSRV: variables ------------------------------------------"
-        yield "TFS_EXTRACT_CMD=tf.exe view /version:%var4% /noprompt \"$%var3%\" /server:%fnvar%(%var2%) /console >%srcsrvtrg%"
-        yield "TFS_EXTRACT_TARGET=%targ%\%var2%%fnbksl%(%var3%)\%var4%\%fnfile%(%var1%)"
-        yield "SRCSRVVERCTRL=tfs"
-        yield "SRCSRVERRDESC=access"
-        yield "SRCSRVERRVAR=var2"
+    let srcsrv =
+        seq {
 
-        for info in infoFiles do
-            let wi = fst info
+            yield "SRCSRV: ini ------------------------------------------------"
+            yield "VERSION=3"
+            yield "INDEXVERSION=2"
+            yield "VERCTRL=Team Foundation Server"
+            yield (sprintf "DATETIME=%s" (DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff")))
+            yield "SRCSRV: variables ------------------------------------------"
+            yield "TFS_EXTRACT_CMD=tf.exe view /version:%var4% /noprompt \"$%var3%\" /server:%fnvar%(%var2%) /console >%srcsrvtrg%"
+            yield "TFS_EXTRACT_TARGET=%targ%\%var2%%fnbksl%(%var3%)\%var4%\%fnfile%(%var1%)"
+            yield "SRCSRVVERCTRL=tfs"
+            yield "SRCSRVERRDESC=access"
+            yield "SRCSRVERRVAR=var2"
 
-            if wi = null then
-                raise (NotFoundInTFS (snd info |> Seq.toList))
+            let infoFilesInTfs =
+                infoFiles
+                |> Seq.where( fun info -> (fst info) <> null)
 
-            yield (sprintf "%s=%s" (serverName wi)) (wi.ServerUri.ToString())
+            for info in infoFiles do
+                let wi = fst info
+                if wi = null then
+                    for file in (snd info) do
+                        printfn " - Arquivo %s não encontrado no TFS." file
+                else
+                    yield (sprintf "%s=%s" (serverName wi)) (wi.ServerUri.ToString())
 
-        yield "SRCSRVTRG=%TFS_extract_target%"
-        yield "SRCSRVCMD=%TFS_extract_cmd%"
-        yield "SRCSRV: source files ---------------------------------------"
+            yield "SRCSRVTRG=%TFS_extract_target%"
+            yield "SRCSRVCMD=%TFS_extract_cmd%"
+            yield "SRCSRV: source files ---------------------------------------"
 
-        for info in infoFiles do
-            let wi = fst info
-            use tpc = new TfsTeamProjectCollection(wi.ServerUri)
-            let vcs = tpc.GetService<VersionControlServer>()
-            let wsp = vcs.GetWorkspace(wi)
+            for info in infoFilesInTfs do
+                let wi = fst info
+                use tpc = new TfsTeamProjectCollection(wi.ServerUri)
+                let vcs = tpc.GetService<VersionControlServer>()
+                let wsp = vcs.GetWorkspace(wi)
 
-            let specs =
-                snd info
-                |> Seq.map (fun file -> ItemSpec(file, RecursionType.None))
-                |> Seq.toArray
+                let specs =
+                    snd info
+                    |> Seq.map (fun file -> ItemSpec(file, RecursionType.None))
+                    |> Seq.toArray
 
-            let locals =
-                wsp.GetLocalVersions(specs, false)
-                |> Seq.collect id
+                let penddingChanges =
+                    wsp.GetPendingChanges(specs)
 
-            for local in locals do
-                yield (sprintf "%s*%s*%s*%d" local.Item (serverName wi) (wsp.GetServerItemForLocalItem local.Item) local.Version)
+                for pend in penddingChanges do
+                    printfn " - Arquivo %s está em checkout e não será indexado" pend.LocalItem
 
-        yield "SRCSRV: end ------------------------------------------------"
-    }
+                let isPedding (item: string) =
+                    let lowerItem = item.ToLower()
+                    penddingChanges
+                    |> Seq.exists (fun pend -> pend.LocalItem.ToLower() = lowerItem)
+
+                let locals =
+                    wsp.GetLocalVersions(specs, false)
+                    |> Seq.collect id
+                    |> Seq.where (fun local -> not (isPedding local.Item))
+
+                for local in locals do
+                    indexed := true
+                    yield (sprintf "%s*%s*%s*%d" local.Item (serverName wi) (wsp.GetServerItemForLocalItem local.Item) local.Version)
+
+            yield "SRCSRV: end ------------------------------------------------"
+        }
+        |> Seq.toList
+
+    srcsrv,!indexed
 
 let writeSrcsrv pdb =
     let tempFile = Path.Combine((Environment.GetEnvironmentVariable("TEMP")), (Guid.NewGuid().ToString() + ".txt"))
 
-    File.AppendAllLines(tempFile, (makeSrcsrv pdb))
 
-    try
-        let arguments = (sprintf "-w -p:%s -s:srcsrv -i:%s" pdb tempFile)
-        execute pdbstrPath arguments
-    finally
-        File.Delete(tempFile)
+    match makeSrcsrv pdb with
+        | _,false -> false
+        | srcsrv,true ->
+            File.AppendAllLines(tempFile, srcsrv)
+
+            try
+                let arguments = (sprintf "-w -p:%s -s:srcsrv -i:%s" pdb tempFile)
+                execute pdbstrPath arguments
+            finally
+                File.Delete(tempFile)
+            true
+
 
 let regPattern = Regex(@"^(.*\\)?([^\\]+)$")
 for filePattern in Seq.skip 1 fsi.CommandLineArgs do
     let mat = regPattern.Match(filePattern)
 
     for pdbFile in Directory.EnumerateFiles((if mat.Groups.[1].Length = 0 then "." else mat.Groups.[1].Value), mat.Groups.[2].Value) do
-        printf "[__] Indexando arquivo %s..." pdbFile
-        try
-            writeSrcsrv pdbFile
-            printfn "\r[OK] Indexando arquivo %s..." pdbFile
-        with
-            | NotFoundInTFS notFoundFiles ->
-                printfn "\r[ERRO] Não foi possível indexar arquivo %s" pdbFile
-                printfn "Os seguintes arquivos não foram encontrados no TFS:"
-                for notFoundFile in notFoundFiles do
-                    printfn " - %s" notFoundFile
+        printfn "Indexando arquivo %s..." pdbFile
+        if writeSrcsrv pdbFile then
+            printfn "Arquivo %s indexado" pdbFile
+        else
+            printfn "Arquivo %s não foi indexado" pdbFile
 
