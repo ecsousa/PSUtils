477487a15dd6521252ca79bad4bab9bdf8644966
Eduardo Antunes Campos de Sousa <eacsousa@yahoo.com.br>
2014-04-29 03:01:33 +0000

    Script para executar programar com elevação
    git-tfs-id: [https://nest.visualstudio.com/defaultcollection]$/PSUtils;C125

---
+++ Start-Elevated.ps1
@@ -0,0 +1,30 @@
+
+function Start-Elevated {
+
+    if($args.Length -eq 0) {
+        Write-Warning "You must provived to program to be executed and its command line arguments"
+    }
+
+    $program = $args[0]
+
+    $alias = Get-Alias $program -ErrorAction SilentlyContinue
+    while($alias) {
+        $program = $alias.Definition;
+        $alias = Get-Alias $program -ErrorAction SilentlyContinue
+    }
+
+    if($args.Length -eq 1) {
+        $cmdLine = '';
+    }
+    else {
+        $cmdLine = [string]::Join(' ', ($args[1..$args.Length] | % { '"' + (([string] $_).Replace('"', '""')) + '"' }) )
+    }
+
+    $psi = new-object System.Diagnostics.ProcessStartInfo
+    $psi.FileName = $program
+    $psi.Arguments = $cmdLine
+    $psi.Verb = "runas"
+
+    [System.Diagnostics.Process]::Start($psi)
+
+}
