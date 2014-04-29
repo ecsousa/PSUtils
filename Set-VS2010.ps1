dd3b471d8b34145f9ba2b9bf17e672fc490f6718
Eduardo Antunes Campos de Sousa <eacsousa@yahoo.com.br>
2014-04-29 02:11:07 +0000

    VS 2012 environment script
    git-tfs-id: [https://nest.visualstudio.com/defaultcollection]$/PSUtils;C124

---
+++ Set-VS2010.ps1
@@ -0,0 +1,24 @@
+function Set-VS2010 {
+    if(-not($env:VS100COMNTOOLS)) {
+        Write-Warning "Environment variable VS100COMNTOOLS is undefined"
+        return;
+    }
+
+    $vsvars32FullPath = Join-Path $env:VS100COMNTOOLS "vsvars32.bat"
+
+    if(-not(Test-Path $vsvars32FullPath)) {
+        Write-Warning "Could not find file '$vsvars32FullPath'";
+        return;
+    }
+
+    Write-Verbose "Importing Visual Studio environment variables from '$vsvars32FullPath'";
+
+        
+    $cmd = "`"$vsvars32FullPath`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+
+    $Global:TitlePrefix = '[vs2010] ';
+}
