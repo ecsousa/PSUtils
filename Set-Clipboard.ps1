7f46bee93d03c28bc71c67b89e8541037aab38e7
Eduardo Antunes Campos de Sousa <eacsousa@yahoo.com.br>
2014-04-29 02:07:36 +0000

    Arquivo para clipboard
    git-tfs-id: [https://nest.visualstudio.com/defaultcollection]$/PSUtils;C123

---
+++ Set-Clipboard.ps1
@@ -0,0 +1,11 @@
+Add-Type -AssemblyName 'System.Windows.Forms'
+
+function Set-Clipboard {
+    $str = $input | Out-String
+
+    [Windows.Forms.Clipboard]::Clear();
+
+    if ( ($str -ne $null) -and ($str -ne '') ) {
+        [Windows.Forms.Clipboard]::SetText( $str )
+    }
+}
