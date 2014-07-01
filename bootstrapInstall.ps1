8f47ec231cbe1c9b0f35b31aaa0a6a0d39f3b9b3
ecsousa <ecsousa@gmail.com>
2014-07-01 02:43:46 -0300

    Correcting push to pushd

--- bootstrapInstall.ps1
+++ bootstrapInstall.ps1
@@ -37,12 +37,12 @@ if(-not(Test-Path ConEmu)) {
 }
 
 if($env:PROCESSOR_ARCHITECTURE -eq 'x64') {
-    push .\ConEmu
+    pushd .\ConEmu
     & .\ConEmu\ConEmu64
     popd
 }
 else {
-    push .\ConEmu
+    pushd .\ConEmu
     & .\ConEmu\ConEmu32
     popd
 }
