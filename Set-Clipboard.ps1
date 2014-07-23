bd5d83ff961ca03c764400b4fceda0bd2756feb8
ecsousa <ecsousa@gmail.com>
2014-07-23 18:15:38 -0300

    Fix ending newline in text copied by Set-Clipboard

--- Set-Clipboard.ps1
+++ Set-Clipboard.ps1
@@ -3,6 +3,8 @@ Add-Type -AssemblyName 'System.Windows.Forms'
 function Set-Clipboard {
     $str = $input | Out-String
 
+    $str = ([regex] (([regex]::Escape([Environment]::NewLine)) + '$')).Replace( $str , '')
+
     [Windows.Forms.Clipboard]::Clear();
 
     if ( ($str -ne $null) -and ($str -ne '') ) {
