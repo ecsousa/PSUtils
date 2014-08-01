7b9e76b8638f7398bf098d998c1e7eb123832fcc
ecsousa <ecsousa@gmail.com>
2014-08-01 01:13:32 -0300

    Revert "If ConEmu.xml exists on AppData folder, move it to PSUtils to keep it clean"
    This reverts commit 54e9f4d80f3694501b655e7015027075849d52a2.

--- PSUtils.psm1
+++ PSUtils.psm1
@@ -283,10 +283,4 @@ if(Test-Path $msgFile) {
     
 }
 
-$conemuXml = Join-Path ([Environment]::GetFolderPath('ApplicationData')) 'PSUtils\ConEmu.xml'
-
-if(Test-Path $conemuXml) {
-    mv $conemuXml (Join-Path $PSScriptRoot 'ConEmu.xml') -Force
-}
-
 
