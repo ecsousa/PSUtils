4f3b271276074212a00677d83c4b144c3b3a2a30
ecsousa <ecsousa@gmail.com>
2014-08-02 02:38:30 -0300

    Git invoke script (try to find Git, and expand PSDrives)

---
+++ Invoke-Git.ps1
@@ -0,0 +1,26 @@
+
+function Invoke-Git {
+    $paths = & {
+        $env:Path.Split(';');
+        Join-Path $Env:ProgramFiles Git\bin;
+        if(${Env:ProgramFiles(x86)}) {
+            Join-Path ${Env:ProgramFiles(x86)} Git\bin;
+        }
+    } | % {
+        Join-Path $_ git.exe
+    } | ? {
+        Test-Path $_
+    }
+
+    if(-not $paths) {
+        Write-Warning "Could not find git.exe"
+        return
+    }
+
+    Write-Verbose "Invoking git.exe from $(@($paths)[0])"
+
+    & (@($paths)[0]) (Expand-Arguments $args | Resolve-PSDrive)
+
+}
+
+
