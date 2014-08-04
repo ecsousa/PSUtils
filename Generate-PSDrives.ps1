a4e814dd914c719193b242122766d8fa278b04b3
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2014-08-04 11:05:37 -0300

    Correcting Generate-PSDrive for testing psdrives.txt existence, and rename PSUtils PSDrive psu

--- Generate-PSDrives.ps1
+++ Generate-PSDrives.ps1
@@ -1,31 +1,22 @@
 $file = Join-Path $env:UserProfile psdrives.txt
 
-if(Test-Path $file) {
+$reg = [regex] '^(\S+)\s+(\S.*)'
 
-    Write-Verbose "[PSUtils] Using drives files from $file"
+foreach($linha in & {if(Test-Path($file)) { gc $file; }; "psu $PSScriptRoot"; } ) { 
+    $m = $reg.Match($linha)
 
-    $reg = [regex] '^(\S+)\s+(\S.*)'
+    if($m.Success) {
+        $prefix = $m.Groups[1].Value;
+        $path = $m.Groups[2].Value;
 
-    foreach($linha in & {gc $file; "PSUtils $PSScriptRoot"; } ) { 
-        $m = $reg.Match($linha)
+        Write-Verbose "[PSUtils] Mapping ${prefix}: PSDrive to $path"
 
-        if($m.Success) {
-            $prefix = $m.Groups[1].Value;
-            $path = $m.Groups[2].Value;
-
-            Write-Verbose "[PSUtils] Mapping ${prefix}: PSDrive to $path"
-
-            "get-psdrive $prefix -ErrorAction SilentlyContinue | Remove-PsDrive"
-            "New-PSDrive -PSProvider FileSystem -Scope Global -Name $prefix -Root '$path' | out-null"
-            "function Set-Location_$prefix { cd ${prefix}: }"
-            "Set-Alias '${prefix}:' 'Set-Location_$prefix'"
-            "Export-ModuleMember -function Set-Location_$prefix"
-            "Export-ModuleMember -alias ${prefix}:"
-        }
+        "get-psdrive $prefix -ErrorAction SilentlyContinue | Remove-PsDrive"
+        "New-PSDrive -PSProvider FileSystem -Scope Global -Name $prefix -Root '$path' | out-null"
+        "function Set-Location_$prefix { cd ${prefix}: }"
+        "Set-Alias '${prefix}:' 'Set-Location_$prefix'"
+        "Export-ModuleMember -function Set-Location_$prefix"
+        "Export-ModuleMember -alias ${prefix}:"
     }
-
-}
-else {
-    Write-Verbose "[PSUtils] No drives files found in $file"
 }
 
