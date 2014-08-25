fe69d3f9bd5c15c9f8c25b80427e6d43a44e515d
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2014-08-25 14:56:29 -0300

    Fix for creating SetLocation function and aliases for current PSDrive

--- Generate-PSDrives.ps1
+++ Generate-PSDrives.ps1
@@ -11,17 +11,18 @@ foreach($linha in & {if(Test-Path($file)) { gc $file; }; "psu $PSScriptRoot"; } 
         $prefix = $m.Groups[1].Value.Trim();
         $path = $m.Groups[2].Value.Trim();
 
-        if($prefix.ToLower() -ne $current.ToLower()) {
+        Write-Verbose "[PSUtils] Mapping ${prefix}: PSDrive to $path"
 
-            Write-Verbose "[PSUtils] Mapping ${prefix}: PSDrive to $path"
+        if($prefix.ToLower() -ne $current.ToLower()) {
 
             "get-psdrive $prefix -ErrorAction SilentlyContinue | Remove-PsDrive"
             "New-PSDrive -PSProvider FileSystem -Scope Global -Name $prefix -Root '$path' | out-null"
-            "function Set-Location_$prefix { cd ${prefix}: }"
-            "Set-Alias '${prefix}:' 'Set-Location_$prefix'"
-            "Export-ModuleMember -function Set-Location_$prefix"
-            "Export-ModuleMember -alias ${prefix}:"
         }
+
+        "function Set-Location_$prefix { cd ${prefix}: }"
+        "Set-Alias '${prefix}:' 'Set-Location_$prefix'"
+        "Export-ModuleMember -function Set-Location_$prefix"
+        "Export-ModuleMember -alias ${prefix}:"
     }
 }
 
