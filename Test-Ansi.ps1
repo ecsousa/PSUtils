25b6b9173b71866b25a425b6d151ffbccf52938e
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2015-04-27 12:57:29 -0300

    Changed way to detect ANSI Colors support

--- Test-Ansi.ps1
+++ Test-Ansi.ps1
@@ -1,5 +1,9 @@
 function Test-Ansi {
 
+    if($host.PrivateData.ToString() -eq 'Microsoft.PowerShell.Host.ISE.ISEOptions') {
+        return $false;
+    }
+
     $oldPos = $host.UI.RawUI.CursorPosition.X
 
         Write-Host -NoNewline "$([char](27))[0m" -ForegroundColor ($host.UI.RawUI.BackgroundColor);
